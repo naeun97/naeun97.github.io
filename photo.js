4d84261be6e155299d3328de17fa0fc4f5166199
naeun97 <nepark@posicube.com>
2023-12-20 16:23:26 +0900

    png만 다운되도록 수정

--- photo.js
+++ photo.js
@@ -134,40 +134,39 @@
         context.drawImage(video, 0, 0);
 
 		const png_data = canvas.toDataURL("image/png",1);
-		downloadImage(png_data,".png",0);//5 초 딜레이
+		downloadImage(png_data,".png");
 
 		const jpeg_data = canvas.toDataURL("image/jpeg",1);
-		downloadImage(jpeg_data,".jpeg",5000);
+		downloadImage(jpeg_data,".jpeg");
 	}
 
   	//이미지 다운
-  	function downloadImage(data,format,delay=0) {
-		setTimeout(function(){
-			var a = document.createElement('a');
-			let today = new Date()
+  	function downloadImage(data,format) {
+		var a = document.createElement('a');
+		let today = new Date()
 			
-			let filename=today.getFullYear().toString()+
-			(today.getMonth()+1).toString()
-			+today.getDate().toString()
-			+'_'
-			+today.getHours().toString()
-			+today.getMinutes().toString()
-			+today.getSeconds().toString()
-			+today.getMilliseconds().toString()
+		let filename=today.getFullYear().toString()+
+		(today.getMonth()+1).toString()
+		+today.getDate().toString()
+		+'_'
+		+today.getHours().toString()
+		+today.getMinutes().toString()
+		+today.getSeconds().toString()
+		+today.getMilliseconds().toString()
 
-			if(select!="png"){
-				filename+=".jpeg"
-			  }
-			  else{
-				filename+=".png"
-			  }
+		if(format!="png"){
+			filename+=".jpg"
+		}
+		else{
+			filename+=".png"
+		}
 	  
-			a.href = data;
-			a.download = filename;
-			document.body.appendChild(a);
-			a.click();
-			document.body.removeChild(a);
-		},delay);
+		a.href = data;
+		a.download = filename;
+		document.body.appendChild(a);
+		a.click();
+		document.body.removeChild(a);
+	
   	}
 	window.addEventListener("load", startup, false);
   })();
